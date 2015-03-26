40c18eddc556b98b209bf3b31b9303f8e0307822
skyemoret <skyemoret@gmail.com>
2015-03-26 17:13:15 -0400

    final changes

--- script/script.js
+++ script/script.js
@@ -18,7 +18,15 @@ var svg = d3.select('.canvas')
 var scales = {};
     scales.x = d3.scale.log().range([0,width]);
     scales.y = d3.scale.linear().range([height,0]);
-
+    scaleColor = d3.scale.ordinal()
+        .domain(["Sub-Saharan Africa",
+            "Middle East & North Africa",
+            "Europe & Central Asia",
+            "East Asia & Pacific",
+            "South Asia",
+            "North America",
+            "Latin America & Caribbean", "undefined"])
+        .range(['red','orange','yellow','teal','blue','green','purple', 'gray']);
 
 //Global variables
 var yVariable = "CO2 emissions (kt)",
@@ -29,17 +37,17 @@ var yVariable = "CO2 emissions (kt)",
 //d3.map for metadata
 var metaDataMap = d3.map();
 
-//TODO: create a layout function for a treemap
+//layout function for treemap
 var treemap = d3.layout.treemap()
-    .children(function(d){
+    .children(function(d){  //set children attribute
         return d.values;
     })
-    .value(function(d){
-        return d.data.get(y0);
+    .value(function(d){     //set value attribute
+        return d.data.get(y0);  //value of children determine how we size parents
     })
-    .size([width,height])
+    .size([width,height])   //set size attribute
     .sticky(true)
-    .padding([5,5,5,5]);
+    .padding([2,2,2,2]);
 
 
 
@@ -52,18 +60,66 @@ queue()
 function dataLoaded(err, rows, metadata){
 
     //First, combine "rows" and "metadata", so that each country is assigned to a region
+    rows.forEach(function(row){
+        row.region = metaDataMap.get(row.key);
+    });
 
     //Then create hierarchy based on regions, using d3.nest()
+    var data = d3.nest()
+        .key(function(d){
+            return d.region;
+        })
+        .entries(rows);
 
-    //Finally, perform a treemap layout on the data
+    var root = {
+        key:"regions",
+        values:data
+    };
 
-    //draw(root);
+    draw(root);
 }
 
 function draw(root){
-    //Append <rect> element for each node in the treemap
+    //Finally, perform a treemap layout on the data
+    var nodes = svg.selectAll('.node')
+        .data(treemap(root), function (d){ return d.key;});
 
+    //Append <rect> element for each node in the treemap
+    var nodesEnter = nodes
+        .enter()
+        .append('g')
+        .attr('class',"node")
+        .classed('leaf',function(d){
+            return !(d.children);       //class this node as a leaf node if there are no children
+        })
+        .attr('transform',function(d){
+            return "translate("+d.x+','+d.y+')';
+        });
+
+
+    nodesEnter
+        .append('rect')
+        .attr('width',function(d){return d.dx; })
+        .attr('height',function(d){return d.dy;})
+        .style('fill-opacity',function(d){ return 1-d.depth/8})
+        .style('fill',function(d) {
+        var region = metaDataMap.get(d.key); //cannot find 'key' for undefined
+
+        return scaleColor(region)
+    });
+
+    nodesEnter
+        .each(function(d){                  //for each node in partition label...
+            if(d.dx > 50){                  //make sure rectangle is large enough to hold label
     //Also append <text> label for each tree node that is a leaf
+                d3.select(this).append('text')
+                    .text(d.key)
+                    .attr('dx', d.dx/2)
+                    .attr('dy', d.dy/2)
+                    .attr('text-anchor','middle')
+                    .style('font-size', '60%');
+            }
+        });
 }
 
 function parse(d){
